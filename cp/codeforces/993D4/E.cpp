72f571dea790946699805774b8e86143a3a3ee1c
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2025-02-08 00:13:11 -0500

    (993D4) - A, B, C, D

---
+++ cp/codeforces/993D4/E.cpp
@@ -0,0 +1,23 @@
+// Insane Problem
+// Tags: binary search, greedy, implementation, math, number theory
+// Level: 1300
+#include <iostream>
+
+using namespace std;
+
+#define cpu()                                                                  \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(nullptr);
+typedef long long ll;
+typedef long double lld;
+
+void solve() {}
+
+int main() {
+  cpu();
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+    solve();
+}
---
+++ cp/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define cpu()                                                                  \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(nullptr)
+typedef long long ll;
+typedef long double lld;
+
+#define rep(i, a, b)                                                           \
+  for (ll i = a; i < b; i++)                                                   \
+    ;
+#define rev(i, a, b)                                                           \
+  for (ll i = a; i >= b; i--)                                                  \
+    ;
+
+const int MAX = INT32_MAX;
+const int MIN = INT32_MIN;
+
+void solve() {}
+
+int main() {
+  cpu();
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+    solve();
+}
